2961836feb60341d896493d91e48d71269f50448
Veanty <plmulticraftpl@gmail.com>
2022-05-30 12:28:21 +0200

    Complete the Variables subject

---
+++ js.js
@@ -0,0 +1,22 @@
+// // let name = "Johny";
+// let surname = "Blackwell";
+// let wholeName = name + " " + surname;
+// let age = 22;
+// let message = "Hello" + " " + name + " " + surname + " of age " + age + ".";
+// const birthday = "06.11.2002";
+// console.log(message);
+// console.log(wholeName);
+// console.log(birthday);
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+// let color = COLOR_GREEN;
+// alert(color);
+
+let name = "John";
+let admin = name;
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = admin;
