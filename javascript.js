12c8c059cb811f9e641f87155353737a2c7e39c8
Veanty <plmulticraftpl@gmail.com>
2022-05-31 17:37:25 +0200

    Complete add weather select and theme select

--- javascript.js
+++ javascript.js
@@ -176,19 +176,40 @@ select.addEventListener("change", setWeather);
 
 function setWeather() {
   const choice = select.value;
-  if (choice == "sunny") {
-    paragraph.textContent =
-      "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
-  } else if (choice == "rainy") {
-    paragraph.textContent =
-      "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long";
-  } else if (choice == "snowing") {
-    paragraph.textContent =
-      "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
-  } else if (choice == "overcast") {
-    paragraph.textContent =
-      "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
-  } else {
-    paragraph.textContent = "";
+  console.log(choice);
+  switch (choice) {
+    case "sunny":
+      paragraph.textContent =
+        "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+      break;
+    case "rainy":
+      paragraph.textContent =
+        "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long";
+      break;
+    case "snowing":
+      paragraph.textContent =
+        "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+      break;
+    case "overcast":
+      paragraph.textContent =
+        "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
+      break;
+    default:
+      paragraph.textContent = "";
   }
 }
+
+const selectTheme = document.querySelector("select.theme");
+const html = document.querySelector("html");
+document.body.style.padding = "10px";
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+selectTheme.addEventListener("change", () =>
+  selectTheme.value === "black"
+    ? update("#1F2937", "#F9FAF8")
+    : update("#F9FAF8", "#1F2937")
+);
