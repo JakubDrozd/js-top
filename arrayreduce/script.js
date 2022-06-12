2099979f6083faabd0d7962bb202aeebf946927e
Veanty <plmulticraftpl@gmail.com>
2022-06-12 12:05:31 +0200

    Complete arrayreduce exercises

--- arrayreduce/script.js
+++ arrayreduce/script.js
@@ -5,6 +5,26 @@ const items = [
   { name: "Monitor", price: 100 },
 ];
 
+const people = [
+  { name: "Kyle", age: 26 },
+  { name: "John", age: 31 },
+  { name: "Sally", age: 42 },
+  { name: "Jill", age: 42 },
+];
+
 const totalPrice = items.reduce((total, item) => {
-  return (total += item.price);
+  return total + item.price;
+}, 0);
+
+const sameAge = people.reduce((groupedPeople, person) => {
+  const age = person.age;
+  if (groupedPeople[age] == null) groupedPeople[age] = [];
+  groupedPeople[age].push(person);
+  return groupedPeople;
+}, {});
+
+const numbers = [13, 2, 5];
+
+const sumNumbers = numbers.reduce((sum, number) => {
+  return sum + number;
 }, 0);
