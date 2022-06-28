d776609f70003ca2a25587aea8be822704c0ac8e
Veanty <plmulticraftpl@gmail.com>
2022-06-28 18:41:17 +0200

    Add some prototype examples

--- ObjectsAndConstructors/script.js
+++ ObjectsAndConstructors/script.js
@@ -1,33 +1,22 @@
-const animalMethods = {
-  eat(amount) {
-    console.log(`${this.name} is eating.`);
-    this.energy += amount;
-  },
-  sleep(length) {
-    console.log(`${this.name} is sleeping`);
-    this.energy += length;
-  },
-  play(length) {
-    console.log(`${this.name} is playing`);
-    this.energy -= length;
-  },
-};
-
 function Animal(name, energy) {
-  let animal = Object.create(animalMethods);
-  animal.name = name;
-  animal.energy = energy;
+  this.name = name;
+  this.energy = energy;
 }
 
-const leo = Animal("Leo", 7);
-const snoop = Animal("Snoop", 10);
-
-const parent = {
-  name: "Stacey",
-  age: 35,
-  heritage: "Irish",
+Animal.prototype.eat = function (amount) {
+  onsole.log(`${this.name} is eating.`);
+  this.energy += amount;
 };
 
-const child = Object.create(parent);
-child.name = "Ryan";
-child.age = 7;
+Animal.prototype.sleep = function (length) {
+  console.log(`${this.name} is sleeping`);
+  this.energy += length;
+};
+
+Animal.prototype.play = function (length) {
+  console.log(`${this.name} is playing`);
+  this.energy -= length;
+};
+
+const leo = new Animal("Leo", 7);
+const snoop = new Animal("Snoop", 10);
