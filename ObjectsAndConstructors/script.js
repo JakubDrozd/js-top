f506c600b05fc91a08110450cc96f13a78f8275f
Veanty <plmulticraftpl@gmail.com>
2022-06-28 17:40:33 +0200

    Add some prototype examples

--- ObjectsAndConstructors/script.js
+++ ObjectsAndConstructors/script.js
@@ -1,15 +1,19 @@
 const cat = {
+  init: function (sound) {
+    this.sound = sound;
+    return this;
+  },
   makeSound: function () {
     console.log(this.sound);
   },
 };
 
 const mark = Object.create(cat);
-mark.sound = "meow";
+mark.init("meow");
 mark.makeSound();
 
 const waffles = Object.create(cat);
-waffles.sound = "meoooooow";
+waffles.init("meooooow!");
 waffles.makeSound();
 
 console.log(`Is mark a cat? ${cat.isPrototypeOf(mark)}`);
