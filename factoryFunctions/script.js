198a58b15704183a4785a8135a2b2c6d3296fd34
Veanty <plmulticraftpl@gmail.com>
2022-07-01 11:37:50 +0200

    Feat new private variables and getters/setters

--- factoryFunctions/script.js
+++ factoryFunctions/script.js
@@ -59,3 +59,68 @@ const Player = (name, level) => {
 
 const veanty = Player("Veanty", 19);
 const wanod = Player("Wanod", 9);
+
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log("draw");
+    },
+  };
+}
+
+const circle1 = createCircle(2);
+const circle2 = createCircle(15);
+
+//Private variables
+
+let obj = (function () {
+  let _prop1 = "Starman";
+  return {
+    prop2: 1981,
+    get property1() {
+      return _prop1;
+    },
+    set prop1(value) {
+      prop1 = value;
+    },
+  };
+})();
+
+for (let prop in obj) {
+  console.log(prop);
+}
+
+console.log(obj.prop2);
+
+console.log(obj.property1);
+
+obj.prop1 = "The Big Lebowski";
+console.log(obj.property1);
+
+console.log(obj._prop1);
+
+//Getters and setters
+
+const person = {
+  firstName: "Jakub",
+  lastName: "Drozd",
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+  set fullName(value) {
+    const parts = value.split(" ");
+    this.firsName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+
+person.fullName = "John Smith";
+
+console.log(person);
+
+const Person = function (name, age) {
+  this.sayHello = () => console.log("hello!");
+  this.name = name;
+  this.age = age;
+};
