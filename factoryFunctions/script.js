cc0c40654f51ce8a032af9022081819204596f34
Veanty <plmulticraftpl@gmail.com>
2022-07-01 03:14:44 +0200

    Feat new Player object

--- factoryFunctions/script.js
+++ factoryFunctions/script.js
@@ -13,8 +13,49 @@ const piotrek = personFactory("Piotr", 20);
 
 const Module = function () {
   return {
-    myMethod: function () {
-      console.log("myMethod has been called");
+    function() {
+      console.log("Hello");
     },
   };
 };
+
+const FactoryFunction = (string) => {
+  const capitalizeString = () => string.toUpperCase();
+  const printString = () => {
+    console.log(`---${capitalizeString()}---`);
+  };
+  return { printString };
+};
+
+const taco = FactoryFunction("taco");
+
+//Factory function
+
+const Player = (name, level) => {
+  let health = level * 2;
+  const getLevel = () => level;
+  const getName = () => name;
+  const die = () => {
+    // oof
+  };
+  const damage = (x) => {
+    health -= x;
+    if (health <= 0) {
+      die();
+    }
+  };
+  const attack = (enemy) => {
+    if (level < enemy.getLevel()) {
+      damage(1);
+      console.log(`${enemy.getName()} has damaged ${name}`);
+    }
+    if (level > enemy.getLevel()) {
+      enemy.damage(1);
+      console.log(`${name} has damaged ${enemy.getName()}`);
+    }
+  };
+  return { attack, damage, getLevel, getName };
+};
+
+const veanty = Player("Veanty", 19);
+const wanod = Player("Wanod", 9);
