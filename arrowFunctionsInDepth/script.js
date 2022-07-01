493542304ea43155da1dd178d1d9077f7404dfac
Veanty <plmulticraftpl@gmail.com>
2022-07-01 18:40:04 +0200

    Feat a function and a following note on how to not use Arrow Functions

--- arrowFunctionsInDepth/script.js
+++ arrowFunctionsInDepth/script.js
@@ -1,8 +1,8 @@
 const sayHello = (...args) => `Hi I'm ${args[0]} ${args[1]} of age ${args[2]}!`;
 
-setTimeout(() => {
-  console.log("Hello");
-}, 1000);
+// setTimeout(() => {
+//   console.log("Hello");
+// }, 1000);
 
 const sayHello2 = () => console.log("Hello");
 
@@ -15,3 +15,17 @@ const me = {
     console.log(`hello`);
   },
 };
+
+function outer(callback, obj) {
+  callback.call(obj);
+}
+
+function inner() {
+  console.log(this);
+}
+
+//Don't use Arrow functions for:
+// 1. Object methods
+// 2. Prototypes
+// 3. Constructors
+// 4. Event handlers
