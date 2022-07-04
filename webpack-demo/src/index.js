df89181e3d59c71e51f0035c01cc7f5f717a9b05
Veanty <plmulticraftpl@gmail.com>
2022-07-04 14:43:19 +0200

    Chore: add style file

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,16 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./avatar.png";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const icon = new Image();
+  icon.src = Icon;
 
   return element;
 }
