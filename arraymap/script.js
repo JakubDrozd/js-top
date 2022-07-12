1dd048aa030031ab6f983872003e93b438f91e47
Veanty <plmulticraftpl@gmail.com>
2022-07-12 18:00:42 +0200

    Move files into seperate directories

--- arraymap/script.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1, -1, 2, 3];
-
-const items = numbers
-  .filter((number) => number >= 0)
-  .map((number) => {
-    return { value: number };
-  })
-  .filter((object) => object.value > 1)
-  .map((object) => object.value);
