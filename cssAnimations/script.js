e561481abaa03987531e3f9be3f255a1e718dff1
Veanty <plmulticraftpl@gmail.com>
2022-07-12 16:27:50 +0200

    Feat CSS animation basics

---
+++ cssAnimations/script.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body");
+const box = document.querySelector(".box");
+window.addEventListener("click", function () {
+  body.innerHTML += `<div class='box'></div>`;
+});
