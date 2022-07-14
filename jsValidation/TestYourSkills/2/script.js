79bccd51228e125fd3bac538d380581454b92241
Veanty <plmulticraftpl@gmail.com>
2022-07-14 14:13:22 +0200

    Create JS validation with regex

---
+++ jsValidation/TestYourSkills/2/script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+const uname = document.getElementById("uname");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const comment = document.getElementById("comment");
