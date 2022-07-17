ddda80272c38d655135f8455e683cee6d04da4f4
Veanty <plmulticraftpl@gmail.com>
2022-07-17 15:20:17 +0200

    Getting GitHub users' fullname

--- async/script.js
+++ async/script.js
@@ -1,11 +1,36 @@
-async function loadJson(url) {
-  const response = await fetch(url);
-  if (response.status === 200) {
-    let json = response.json();
-    return json;
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = "HttpError";
+    this.response = response;
   }
-
-  throw new Error(response.status);
 }
 
-loadJson("https://javascript.info/no-such-user.json").catch(alert); // Error: 404
+async function loadJson(url) {
+  const response = await fetch(url);
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+// Ask for a user name until github returns a valid user
+async function demoGithubUser() {
+  while (true) {
+    try {
+      let name = prompt("Enter a name?", "iliakan");
+      let user = await loadJson(`https://api.github.com/users/${name}`);
+      alert(`Full name: ${user.name}.`);
+      break;
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("No such user, please reenter.");
+      } else {
+        throw err;
+      }
+    }
+  }
+}
+
+demoGithubUser();
