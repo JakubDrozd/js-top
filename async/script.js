d7d77796e094b94e0134179e1d769659efd351a4
Veanty <plmulticraftpl@gmail.com>
2022-07-17 15:23:42 +0200

    Refactor: delete useless comment

--- async/script.js
+++ async/script.js
@@ -40,9 +40,6 @@ async function wait() {
 
 function f() {
   wait().then((response) => console.log(response));
-  // ...what should you write here?
-  // we need to call async wait() and wait to get 10
-  // remember, we can't use "await"
 }
 
 f();
