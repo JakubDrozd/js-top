226b5e91972fff52305096e56071ffb74b7b9b75
Veanty <plmulticraftpl@gmail.com>
2022-07-17 12:03:05 +0200

    Using promises for api fetches

---
+++ Promise/promise4/script.js
@@ -0,0 +1,10 @@
+const iTunesPromise = axios.get(
+  "https://itunes.apple.com/search?term=alita battle angel&media=music"
+);
+
+iTunesPromise.then(
+  (response) => console.log(response.data),
+  (reason) => console.log(reason)
+);
+
+console.log(`Finished!`);
