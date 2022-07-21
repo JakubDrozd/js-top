05718a3ae10df7ef37ae104004edadfa56574235
Veanty <plmulticraftpl@gmail.com>
2022-07-21 14:00:01 +0200

    Feat: new jest tests for orderTotal function

--- TDD/ordertotal.test.js
+++ TDD/ordertotal.test.js
@@ -7,3 +7,33 @@ it("Quantity", () => {
     })
   ).toBe(6);
 });
+
+it("No quantity specified", () => {
+  expect(
+    orderTotal({
+      items: [{ name: "Dragon candy", price: 3 }],
+    })
+  ).toBe(3);
+});
+
+it("Check fail: Happy path (Example 1)", () => {
+  expect(
+    orderTotal({
+      items: [
+        { name: "Dragon food", price: 8, quantity: 1 },
+        { name: "Dragon cage (small)", price: 800, quantity: 1 },
+      ],
+    })
+  ).toBe(808);
+});
+
+it("Happy path (Example 2)", () => {
+  expect(
+    orderTotal({
+      items: [
+        { name: "Dragon food", price: 20, quantity: 1 },
+        { name: "Dragon cage (small)", price: 40, quantity: 1 },
+      ],
+    })
+  ).toBe(60);
+});
