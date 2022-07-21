b841277bd20f3114752ee0205db212af5782639c
Veanty <plmulticraftpl@gmail.com>
2022-07-21 14:00:24 +0200

    Refactor: now properly calculates orders price with count equal to 1

--- TDD/ordertotal.js
+++ TDD/ordertotal.js
@@ -1,6 +1,6 @@
 function orderTotal(order) {
   return order.items.reduce(
-    (total, item) => total + item.price * item.quantity,
+    (total, item) => total + item.price * (item.quantity || 1),
     0
   );
 }
