f9a056d9c1c6a54c71fc64dfcc6ed4e3bb2ed557
Veanty <plmulticraftpl@gmail.com>
2022-07-24 11:41:14 +0200

    Refactor: change promise to async function

--- Promise/promise5/script.js
+++ Promise/promise5/script.js
@@ -2,42 +2,51 @@ const getUserData = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("1. get user data");
-      resolve();
+      resolve({ name: "Jon" });
     }, 800);
   });
 };
 
-const validateData = () => {
-  return new Promise((resolve, reject) => {
-    reject("ValidationError");
-  });
-};
-
-const registerUser = () => {
+const registerUser = (user) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("3. register");
-      resolve();
+      console.log("2. register");
+      resolve({ name: user.name, id: 1 });
     }, 400);
   });
 };
 
-const sendEmail = () => {
+const sendEmail = (user) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("4. send email");
+      console.log(`3. Email sent by ${user.name} [${user.id}]`);
       resolve();
     }, 200);
   });
 };
 
-getUserData()
-  .then(validateData)
-  .then(registerUser)
-  .then(sendEmail)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((err) => {
-    console.log(`Error: ${err}`);
-  });
+// getUserData()
+//   .then((userData) => {
+//     return registerUser(userData);
+//   })
+//   .then((registeredUser) => {
+//     return sendEmail(registeredUser);
+//   })
+//   .then(() => {
+//     console.log("Email sent");
+//   })
+//   .catch((err) => {
+//     console.log(`Error: ${err}`);
+//   });
+
+async function sendUserEmail() {
+  const userData = await getUserData();
+  const registeredUser = await registerUser(userData);
+  await sendEmail(registeredUser);
+
+  console.log("Operation successful");
+}
+
+sendUserEmail();
+console.log("render page");
+console.log("display page");
