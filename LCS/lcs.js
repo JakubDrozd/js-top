eeb629053e82695f904ab1f0719af84a77d242c4
Veanty <plmulticraftpl@gmail.com>
2022-07-26 17:52:09 +0200

    Feat: longest common substring task

--- LCS/lcs.js
+++ LCS/lcs.js
@@ -1,22 +1,20 @@
-let arr = ["ABC", "ABC"];
+let arr = process.argv.slice(2);
+console.log(arr);
 let n = arr.length;
 let s = arr[0];
 let len = s.length;
 let res = "";
 for (let i = 0; i < len; i++) {
-  for (let j = i; j <= len; j++) {
-    let stem = s.substring(i, j);
+  for (let j = i + 1; j <= len; j++) {
+    let stem = s.substring(i, j - i);
     let k = 1;
-    for (k = 1; k < n; k++) {
+    for (k = 1; k < n; k++)
       if (!arr[k].includes(stem)) {
         break;
       }
-      if (k == n && res.length < stem.length) {
-        res = stem;
-      }
-    }
+
+    if (k == n && res.length < stem.length) res = stem;
   }
 }
 
 console.log(res);
-console.log(n);
