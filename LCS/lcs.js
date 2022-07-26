5a3430b08a86d9e67934b31b1e09f8325e317201
Veanty <plmulticraftpl@gmail.com>
2022-07-26 18:40:09 +0200

    Refactor: change variable names and compress file

--- LCS/lcs.js
+++ LCS/lcs.js
@@ -1,1 +1,16 @@
-let a=process.argv.slice(2);function LCS(n){var o=n.length;let g=n[0];var l=g.length;let r="";for(let t=0;t<l;t++)for(let e=t+1;e<=l;e++){var s=g.substring(t,e-t);let l=1;for(l=1;l<o&&n[l].includes(s);l++);l==o&&r.length<s.length&&(r=s)}console.log(r)}a.length<=0?console.log(""):LCS(a);
+let a = process.argv.slice(2);
+function LCS(n) {
+  var o = n.length;
+  let g = n[0];
+  var l = g.length;
+  let r = "";
+  for (let t = 0; t < l; t++)
+    for (let e = t + 1; e <= l; e++) {
+      var s = g.substring(t, e - t);
+      let l = 1;
+      for (l = 1; l < o && n[l].includes(s); l++);
+      l == o && r.length < s.length && (r = s);
+    }
+  console.log(r);
+}
+a.length <= 0 ? console.log("") : LCS(a);
