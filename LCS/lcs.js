495af4ac0ef287a71d08e9794ccd3c49b927c664
Veanty <plmulticraftpl@gmail.com>
2022-07-26 17:41:39 +0200

    Feat: longest common substring task

--- LCS/lcs.js
+++ LCS/lcs.js
@@ -1,36 +1,22 @@
-function lcsOf3(X, Y, Z, m, n, o) {
-  let L = new Array(m + 1);
-  for (let i = 0; i < m + 1; i++) {
-    L[i] = new Array(n + 1);
-    for (let j = 0; j < n + 1; j++) {
-      L[i][j] = new Array(o + 1);
-      for (let k = 0; k < o + 1; k++) {
-        L[i][j][k] = 0;
+let arr = ["ABC", "ABC"];
+let n = arr.length;
+let s = arr[0];
+let len = s.length;
+let res = "";
+for (let i = 0; i < len; i++) {
+  for (let j = i; j <= len; j++) {
+    let stem = s.substring(i, j);
+    let k = 1;
+    for (k = 1; k < n; k++) {
+      if (!arr[k].includes(stem)) {
+        break;
+      }
+      if (k == n && res.length < stem.length) {
+        res = stem;
       }
     }
   }
-  for (let i = 0; i <= m; i++) {
-    for (let j = 0; j <= n; j++) {
-      for (let k = 0; k <= o; k++) {
-        if (i == 0 || j == 0 || k == 0) L[i][j][k] = 0;
-        else if (X[i - 1] == Y[j - 1] && X[i - 1] == Z[k - 1])
-          L[i][j][k] = L[i - 1][j - 1][k - 1] + 1;
-        else
-          L[i][j][k] = Math.max(
-            Math.max(L[i - 1][j][k], L[i][j - 1][k]),
-            L[i][j][k - 1]
-          );
-      }
-    }
-  }
-  return L[m][n][o];
 }
 
-let X = "KBCC";
-let Y = "BCCD";
-let Z = "ABCC";
-let m = X.length;
-let n = Y.length;
-let o = Z.length;
-
-document.write("Length of LCS is " + lcsOf3(X, Y, Z, m, n, o));
+console.log(res);
+console.log(n);
